3162df10885002a6dc01def496190af718b976f3
656Krona <ykaskrmr@gmail.com>
2020-03-08 08:17:19 +0000

    [Add]devise_User関連のcontrollerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
 # --------------- user ---------------
 # devise
-  devise_for :users
+  devise_for :users, :controllers => {
+     :registrations => 'users/registrations',
+     :sessions => 'users/sessions',
+     :passwords => 'users/passwords'
+    }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
