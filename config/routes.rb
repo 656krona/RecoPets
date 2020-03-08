c5543f5c648c311a9cf6e22364270a6e38d0a7ec
656Krona <ykaskrmr@gmail.com>
2020-03-08 07:59:53 +0000

    [Add]deviseのgem追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+# --------------- user ---------------
+# devise
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
