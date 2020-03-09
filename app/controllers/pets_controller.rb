afe13457a2e902bdf86b968ce658e6565b181181
656Krona <ykaskrmr@gmail.com>
2020-03-09 18:46:52 +0000

    [Add]ペット一覧,登録画面

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,16 +1,24 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pets.all
+    @pets = Pet.all
   end
 
   def new
-    @pet = Pets.new
+    @pet = Pet.new
   end
 
   def create
+    @pet = Pet.new(pet_params)
+    @pet.user_id = current_user.id
+    @pet.save
+    redirect_to pets_path
   end
 
   def destroy
   end
 
+  private
+  def pet_params
+      params.require(:pet).permit(:name, :pet_image)
+  end
 end
