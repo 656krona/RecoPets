de69f813414f9ad6160f09a016a227de17da22bb
656Krona <ykaskrmr@gmail.com>
2020-03-16 17:04:17 +0000

    [Update]コメント一覧画面,非同期通信投稿

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+
+  def index
+    @topic = Topic.find(params[:topic_id])
+    @comments = Comment.where(topic_id: params[:topic_id]).page(params[:page]).reverse_order
+    @categories = Category.where(category_status: 0)
+    @comment = Comment.new(topic_id: @topic.id)
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @comments = @topic.comments
+    @comment = @topic.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      flash[:success] = "コメントが投稿されました"
+    else
+      @comments = Comment.where(id: @topic)
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment, :url, :comment_image)
+  end
+
+
 end
