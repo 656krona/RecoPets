dbd18feebf2e1b738b07d60ce42fe667eac8cfc3
656Krona <ykaskrmr@gmail.com>
2020-03-19 08:10:36 +0000

    [Add]非同期通信のlike機能追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :topic
   has_many :likes, dependent: :destroy
   attachment :comment_image
+
+  def favorited_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
+
 end
