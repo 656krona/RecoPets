0c2c4fde48c1a45457c4c86a2ab7e466416c7ead
656Krona <ykaskrmr@gmail.com>
2020-03-19 06:42:26 +0000

    [Add]itemルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:index, :create, :destroy]
   end
 
+  resources :items, only: [:index, :create, :edit, :update, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
