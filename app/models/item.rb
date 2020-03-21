b7a39acd72fafdec7d21cae70fc5256ae7e2eba4
656Krona <ykaskrmr@gmail.com>
2020-03-21 17:57:40 +0000

    [Fix]残量計算式修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,15 +7,10 @@ class Item < ApplicationRecord
   def remaining_per
     update_elapsed_days = (Date.current - updated_at.to_date).to_i # 更新してからの経過日数
 
-    if amount == 100
-      result = (update_elapsed_days / days) * 100 - 100 # 残在庫
-    elsif amount != 100
-      per_one_day = 100 / days
-      remaining_days = amount / per_one_day #あと何日もつかdaysと同義
-      # result = (amount - (update_elapsed_days / d3.to_f.round(2)) * 100).round
-      result = (update_elapsed_days / remaining_days.to_f.round(2) * 100 - amount).round
-    end
-
+    per_one_day = 100 / days
+    remaining_days = amount / per_one_day #あと何日もつかdaysと同義
+    # result = (amount - (update_elapsed_days / d3.to_f.round(2)) * 100).round
+    result = (update_elapsed_days / remaining_days.to_f.round(2) * 100 - amount).round
     result.round(2).abs # 小数点2位で四捨五入
   end
 
