c53e6f22b15a7fb27c714036c9ad274e2317d0a3
656Krona <ykaskrmr@gmail.com>
2020-03-22 11:14:48 +0000

    [Fix]recordデータ生成計算式修正

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,33 +8,20 @@ class PetsController < ApplicationController
   end
 
   def show
-    require 'date'
-    date = Date.today
-
     @pet = Pet.find(params[:id])
-    if params[:start_date].blank? && @pet.records.where(start_time: date.beginning_of_month.strftime("%Y-%m-%d 15:%M:%S")).empty?
-      firstday = date.beginning_of_month
-      lastday = date.end_of_month
-      #文字列にすると時間を含んだeach文となり無限ループ状態なのでダメ
-      (firstday..lastday).each do |day|
-        @record = Record.new(start_time: day)
-        #binding.pry
-        @record.pet_id = @pet.id
-        @record.save
-      end
-    elsif !params[:start_date].blank? && @pet.records.where(start_time: Date.parse(params[:start_date]).beginning_of_month.strftime("%Y-%m-%d 15:%M:%S")).empty?
-      firstday = Date.parse(params[:start_date]).beginning_of_month
-      lastday = Date.parse(params[:start_date]).end_of_month
-      (firstday..lastday).each do |day|
-        @record = Record.new(start_time: day)
-        @record.pet_id = @pet.id
-        @record.save
-      end
-    else
-    end
+    @pet.create_a_month_records(params[:start_date])
 
     @records = @pet.records
     @user = @pet.user_id
+    plan = Record
+      .where('start_time >= ?', Date.current)
+      .where(content_type: 1)
+      .where(pet_id: @pet)
+      .order(:start_time)
+      .first
+    # 三項演算 ( 式 ? trueの処理 : falseの処理 )
+    @plan_day = plan.present? ? (plan.start_time.to_date - Date.current).to_i : nil
+    #logger.info(@plan_day)
   end
 
   def create
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,18 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :records, dependent: :destroy
   attachment :pet_image
+
+  validates :name, presence: true
+
+  def create_a_month_records(date_str)
+    date = date_str.present? ? date_str.to_date : Date.current
+
+    return if records.where(start_time: date.beginning_of_month.strftime("%Y-%m-%d 15:%M:%S")).present?
+
+    firstday, lastday = date.beginning_of_month, date.end_of_month
+    (firstday..lastday).each do |day|
+
+      records.create(start_time: day)
+    end
+  end
 end
