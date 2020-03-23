d9975acf6630db93416110d8eec25530c228f4c6
656Krona <ykaskrmr@gmail.com>
2020-03-23 08:21:33 +0000

    [Fix]バリデーション

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,7 @@ class Topic < ApplicationRecord
   belongs_to :category
   has_many :comments
   attachment :topic_image
+
+  validates :category_id, presence: {message: "が入力されていません"}
+  validates :title, presence: {message: "が入力されていません"}
 end
