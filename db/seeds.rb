ab27f1ac1fbe9323f86e8b97649ff7a7b45d08ed
656Krona <ykaskrmr@gmail.com>
2020-03-27 15:15:28 +0000

    [Add]ペット,病院データ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,15 @@
 User.create(id:1, name:"yuka", email:"a@a", password:"111111")
 
 Pet.find_or_create_by(id:1, user_id: 1, name:"まろん")
+Pet.find_or_create_by(id:2, user_id: 1, name:"ジョン")
 
 Hospital.find_or_create_by(id:1, name: "もりやま動物病院", postcode: "111-1111", prefecture_name: "埼玉県", address_city: "さいたま市浦和区", address_street: "岸町3丁目5番地", phone: "048-000-0000")
 Hospital.find_or_create_by(id:2, name: "バード・クリニック", postcode: "111-1111", prefecture_name: "鹿児島県", address_city: "鹿児島市", address_street: "和田1丁目1番地", phone: "099-000-0000")
 Hospital.find_or_create_by(id:3, name: "さやま動物病院", postcode: "111-1111", prefecture_name: "埼玉県", address_city: "さいたま市常盤区", address_street: "3丁目5番地", phone: "048-000-0000")
+Hospital.find_or_create_by(id:4, name: "アネラどうぶつ病院", postcode: "111-1111", prefecture_name: "福島県", address_city: "いわき市平大町", address_street: "1丁目3番地", phone: "0246-000-0000")
+Hospital.find_or_create_by(id:5, name: "根本獣医科医院", postcode: "111-1111", prefecture_name: "広島県", address_city: "尾道市栗原町", address_street: "4丁目12番地", phone: "0848-000-0000")
+Hospital.find_or_create_by(id:6, name: "ペットメディカルセンター", postcode: "111-1111", prefecture_name: "沖縄県", address_city: "沖縄県沖縄市", address_street: "松本4丁目2番地", phone: "098-000-0000")
+Hospital.find_or_create_by(id:7, name: "サクラペットクリニック", postcode: "111-1111", prefecture_name: "広島県", address_city: "広島市西区", address_street: "2丁目10番地", phone: "0848-000-0000")
 
 Category.find_or_create_by(id:1, name: "犬")
 Category.find_or_create_by(id:2, name: "猫")
@@ -22,3 +27,5 @@ Category.find_or_create_by(id:5, name: "うさぎ")
 Category.find_or_create_by(id:6, name: "その他小動物")
 Category.find_or_create_by(id:7, name: "爬虫類")
 Category.find_or_create_by(id:8, name: "動物病院")
+
+
