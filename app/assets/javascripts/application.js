8a804ea1a68354ed8d02da46d6dbbac5003a9647
656Krona <ykaskrmr@gmail.com>
2020-05-04 19:06:58 +0000

    [Add]jqueryコード

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,42 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+
+$(function(){
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        if($("#img_prev").prop("tagName") == 'div'){
+          var html = `<img src="${e.target.result}" width="300" height="200">`
+          $('#img-prev').append(html);
+        }else{
+          //src属性を書き換える
+          $('#img-prev').attr('src', e.target.result);
+        }
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#post_img").change(function(){
+    readURL(this);
+  });
+})
+
+$(function() {
+  $('#back a').on('click',function(){
+    $('body, html').animate({
+      scrollTop:0
+      }, 800);
+      return false;
+  });
+});
+
+$(function() {
+  $('#comment-form button').on('click',function(){
+    $('body, html').animate({
+      scrollTop: $(document).height()
+      }, 1500);
+      return false;
+  });
+});
