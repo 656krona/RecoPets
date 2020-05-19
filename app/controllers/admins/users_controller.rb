676bf33263017bfb3f76b89ff40eb7791e4a2660
656Krona <ykaskrmr@gmail.com>
2020-05-19 13:16:01 +0000

    [Add]管理者側usersコントローラ追加

---
+++ app/controllers/admins/users_controller.rb
@@ -0,0 +1,18 @@
+class Admins::UsersController < ApplicationController
+
+  def index
+    @users = User.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
+end
